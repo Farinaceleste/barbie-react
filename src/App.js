4c68b64040dee9bb05104e296c3f561e51ed9c2e
Farinaceleste <140749549+Farinaceleste@users.noreply.github.com>
2023-12-08 21:45:11 -0300

    se comenta bulma que rompe build

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { ButtonList } from "./components/ButonList/ButonList";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer"
 import './App.css';
-import "bulma/css/bulma.css";
+// import "bulma/css/bulma.css";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
