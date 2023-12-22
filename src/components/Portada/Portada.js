746adf58d02763bd5e8572ff2574323349cdd9ef
farinaceleste <farinaceleste@gmail.com>
2023-12-22 20:51:01 -0300

    cambios en imagenes, agregue estilos css

--- src/components/Portada/Portada.js
+++ src/components/Portada/Portada.js
@@ -4,32 +4,32 @@ import FotoPortada from "./foto-portada.jpg";
 import Carduno from "./barbie7.jpg";
 import Carddos from "./barbie8.jpg";
 import Cardtres from "./barbie10.jpg";
-import { Link } from "react-router-dom";
+
 
 function Portada() {
     return (
         <div>
             <div className="portada">
-                <div className="imagen-portada">
-                    <img src={FotoPortada} />
+                <div className="imagen-portada-div" >
+                    <img className="imagen-portada" src={FotoPortada} alt="card-portada" />
                 </div>
                 <div className="texto-portada">
                     <h3>WE ARE</h3>
-                    <img className="letra-logo" src={LetraLogo} />
+                    <img className="letra-logo" src={LetraLogo} alt="card-portada"/>
                 </div>
             </div>
             <div className="cards-portada">
                 <div className="card">
-                    <img className="imagen-card" src={Carduno}/>
-                    <p className="texto-card"><Link to ="/productos">TOP-RATED BARBIE GIFTS</Link></p>
+                    <img className="imagen-card" src={Carduno} alt="card-portada"/>
+                    <p className="texto-card">TOP-RATED BARBIE GIFTS</p>
                 </div>
                 <div className="card">
-                    <img className="imagen-card" src={Carddos}/>
-                    <p className="texto-card"><Link to ="/productos">THE FUTURE IS PINK</Link></p>
+                    <img className="imagen-card" src={Carddos} alt="card-portada"/>
+                    <p className="texto-card">THE FUTURE IS PINK</p>
                 </div>
                 <div className="card">
-                    <img className="imagen-card" src={Cardtres}/>
-                    <p className="texto-card"><Link to ="/productos">YOU CAN BE EVERYTHING</Link></p>
+                    <img className="imagen-card" src={Cardtres} alt="card-portada"/>
+                    <p className="texto-card">YOU CAN BE EVERYTHING</p>
                 </div>
             </div>
         </div>
